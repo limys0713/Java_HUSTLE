c89221e4ecdf3054f4abd942488a5e4cdb24bf9d
KylanLim <yiksenglim6@gmail.com>
2025-05-24 22:18:26 +0800

    Switch Statement
    New version that for those cases that their output is same can be like case 1, case 2:

---
+++ footballMatchReports/src/footballMatchReports/Main.java
@@ -0,0 +1,13 @@
+package footballMatchReports;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Task 1
+		System.out.println(FootballMatchReports.onField(10));
+		// Task 2
+		System.out.println(FootballMatchReports.onField(13));
+	}
+
+}
